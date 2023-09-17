2252c5a40de40628686db93cc9002b3319b20ef9
lampat26 <115820749+lampat26@users.noreply.github.com>
2023-09-17 16:28:26 -0400

    Exercise 04

--- assignment/exercise04.py
+++ assignment/exercise04.py
@@ -4,29 +4,38 @@ Use analog input with photocell
 
 import time
 import machine
+import json
+import os
+
+
+def get_params(param_file: str) -> dict:
+    """Reads parameters from a JSON file."""
+    
+    with open(param_file) as f:
+        params = json.load(f)
+
+    return params
 
 led = machine.Pin("LED", machine.Pin.OUT)
 adc = machine.ADC(28)
 
 blink_period = 0.1
 
-max_bright = 20000
-min_bright = 10000
+params = get_params("exercise04.json")
+
+print("this is params")
+print(params)
+
+max_bright = params["max_bright"]
+min_bright = params["min_bright"]
 
 while True:
     value = adc.read_u16()
-    print(value)
-    # %% need to clip duty cycle to range [0, 1]
-
+    #print(value)
     duty_cycle = (value - min_bright) / (max_bright - min_bright)
-    # this equation will give values outside the range [0, 1]
-
-    # %% clip duty cycle to range [0, 1]
-    if duty_cycle < 0:
-        duty_cycle = 0
-    elif duty_cycle > 1:
-        duty_cycle = 1
-
+    
+    print(duty_cycle)
+    
     led.high()
     time.sleep(blink_period * duty_cycle)
     led.low()
