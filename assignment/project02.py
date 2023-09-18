ada06377ee447f56715d76180f1b55bbafadbd6a
Steven Cheng <stcheng@bu.edu>
2023-09-18 18:38:03 -0400

    scorerMulti, get_params

--- assignment/project02.py
+++ assignment/project02.py
@@ -8,12 +8,11 @@ use two simultaneously running threads to:
 import machine
 import time
 import _thread
-import exercise04
+import json
 
 import project01
 
 # project01.py also needs to be copied to the Pico
-
 
 def photocell_logger(N: int, sample_interval_s: float) -> None:
     """
@@ -56,6 +55,50 @@ def photocell_logger(N: int, sample_interval_s: float) -> None:
 
     project01.write_json(filename, data)
 
+def get_params(param_file: str) -> dict:
+    """Reads parameters from a JSON file."""
+    
+    with open(param_file) as f:
+        params = json.load(f)
+
+    return params
+
+def scorerMulti(t: list[int | None]) -> None:
+    # %% collate results
+    for i in len(t):
+        misses = t[i].count(None)
+        print(f"Player {i}: You missed the light {misses} / {len(t)} times")
+
+        t_good = [x for x in t[i] if x is not None]
+
+        max_t = max(t_good)
+        min_t = min(t_good)
+        avg_t = sum(t_good)/len(t_good)
+        score = len(t_good)/N
+        
+        print(t_good)
+        print(max_t)
+        print(min_t)
+        print(avg_t)
+
+        # add key, value to this dict to store the minimum, maximum, average response time
+        # and score (non-misses / total flashes) i.e. the score a floating point number
+        # is in range [0..1]
+        data[i] = {"response times" : t_good,
+                "max_time" : max_t,
+                "min_time" : min_t,
+                "average_time" : avg_t,
+                "score" : score
+                }
+
+        # %% make dynamic filename and write JSON
+
+    filename = f"proj2-light.json"
+
+    print("write", filename)
+
+    write_json(filename, data)
+
 
 def blinker_response_game(N: int) -> None:
     # %% setup input and output pins
@@ -64,31 +107,45 @@ def blinker_response_game(N: int) -> None:
     button2 = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
 
     # %% please read these parameters from JSON file like project 01 instead of hard-coding
-    sample_ms, on_ms = exercise04.get_params("project02.json")
+    sample_ms, on_ms = get_params("project02.json")
 
-    t: list[float | None] = []
-
+    t1: list[int | None] = []
+    t2: list[int | None] = []
+    
     project01.blinker(3, led)
-
+    
     for i in range(N):
         time.sleep(project01.random_time_interval(0.5, 5.0))
 
         led.high()
 
         tic = time.ticks_ms()
-        t0 = None
+        ta = None
+        tb = None
+
         while time.ticks_diff(time.ticks_ms(), tic) < on_ms:
-            if button.value() == 0:
-                t0 = time.ticks_diff(time.ticks_ms(), tic)
+            
+            if button1.value() == 0:
+                ta = time.ticks_diff(time.ticks_ms(), tic)
                 led.low()
+                
+            if button2.value() == 0:
+                tb = time.ticks_diff(time.ticks_ms(), tic)
+                led.low()
+                
+            print(ta)
+            if ta is not None and tb is not None:
                 break
-        t.append(t0)
+            
+        t1.append(ta)
+        t2.append(tb)
 
         led.low()
 
     project01.blinker(5, led)
 
-    project01.scorer(t)
+    t = [t1, t2]
+    scorerMulti(t)
 
 
 _thread.start_new_thread(photocell_logger, (10, 0.5))
